aa9a94b5e508b9643e082a79f4e0e7fad6f7c1a9
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2025-01-24 11:59:49 +0300

    Update Calculator.java

--- src/Task3/Calculator.java
+++ src/Task3/Calculator.java
@@ -1,7 +1,12 @@
 // Класс для выполнения математических операций
 public class Calculator {
+
     // Метод для сложения
     public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b) {
         return a + b;
     }
 
@@ -10,8 +15,16 @@ public class Calculator {
         return a - b;
     }
 
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
     // Метод для умножения
     public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static int multiply(int a, int b) {
         return a * b;
     }
 
@@ -23,8 +36,19 @@ public class Calculator {
         return a / b;
     }
 
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Деление на ноль невозможно.");
+        }
+        return a / b;
+    }
+
     // Метод для взятия процента от числа
     public static double percentage(double a, double percent) {
+        return (a * percent) / 100;
+    }
+
+    public static int percentage(int a, int percent) {
         return (a * percent) / 100;
     }
 }
